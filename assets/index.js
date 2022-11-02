6437311d5f65286d1e4a074ff410b528eb1cb90a
Tobi-De <tobidegnon@protonmail.com>
2022-11-02 17:49:41 +0100

    feat: responsive navbar, look decent on mobile

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,14 @@ import "./styles.css";
 import "./code.css"
 import 'htmx.org';
 import 'giscus';
+import Alpine from 'alpinejs'
 
 import mermaidAPI from 'mermaid';
 
 window.htmx = require('htmx.org');
 
-mermaidAPI.initialize({'theme': 'dark', startOnLoad: true });
+mermaidAPI.initialize({'theme': 'dark', startOnLoad: true});
+
+window.Alpine = Alpine
+
+Alpine.start()
