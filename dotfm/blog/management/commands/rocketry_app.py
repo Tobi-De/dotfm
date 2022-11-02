c9d9bf7576d48bd15ab2a1bfc5b8d9706bb9e659
Tobi-De <tobidegnon@protonmail.com>
2022-11-02 15:39:20 +0100

    chore: setup database monthly auto backup with rocketry
    Resolves: #32

---
+++ dotfm/blog/management/commands/rocketry_app.py
@@ -0,0 +1,18 @@
+import subprocess
+
+from django.core.management.base import BaseCommand
+from rocketry import Rocketry
+
+app = Rocketry(config={"task_execution": "main"})
+
+
+@app.task("monthly")
+def backup_database():
+    subprocess.run(["python", "manage.py", "pgclone", "dump"])
+
+
+class Command(BaseCommand):
+    help = "Run the rocketry worker"
+
+    def handle(self, *args, **options):
+        app.run()
