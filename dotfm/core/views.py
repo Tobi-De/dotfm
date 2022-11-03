ea7a2cb44cdb448e667e9cf5c8015027923ec80a
Tobi-De <tobidegnon@protonmail.com>
2022-11-03 08:55:24 +0100

    build: rename worker to scheduler since rocketry is what I use it for

--- dotfm/core/views.py
+++ dotfm/core/views.py
@@ -1,12 +1,12 @@
 from django.conf import settings
-from django.http import FileResponse, HttpRequest, HttpResponse
+from django.http import FileResponse, HttpRequest
 from django.views.decorators.cache import cache_control
 from django.views.decorators.http import require_GET
 
 
 @require_GET
 @cache_control(max_age=60 * 60 * 24, immutable=True, public=True)  # one day
-def favicon_file(request: HttpRequest) -> HttpResponse:
+def favicon_file(request: HttpRequest) -> FileResponse:
     name = request.path.lstrip("/")
     file = (settings.APPS_DIR / "static" / "favicons" / name).open("rb")
     return FileResponse(file)
