7766c205c8f8e4aac8cd533fb4185966521952cf
Tobi-De <tobidegnon@protonmail.com>
2022-11-07 12:23:09 +0100

    feat: add alternative rss feed path

--- dotfm/blog/urls.py
+++ dotfm/blog/urls.py
@@ -6,6 +6,7 @@ from .views import content
 app_name = "blog"
 
 urlpatterns = [
+    path("feed/", ContentFeed()),
     path("rss.xml", ContentFeed()),
     re_path(r"^(?P<slug>(\w|-|\/)*)", content, name="content"),
 ]
