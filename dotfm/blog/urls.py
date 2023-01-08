9aed105a039a3cbd6a23f7caa95b4c2cd20fb2bc
Tobi DEGNON <tobidegnon@proton.me>
2023-01-08 21:44:58 +0100

    fix: redirect blog index on posts list

--- dotfm/blog/urls.py
+++ dotfm/blog/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, reverse
 
 from .feed import AtomFeed, RssFeed
 from .views import content
+from django.views.generic import RedirectView
 
 app_name = "blog"
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/blog/posts"), name="index"),
     path("rss.xml/", RssFeed(), name="rss_feed"),
     path("atom.xml/", AtomFeed(), name="atom_feed"),
     re_path(r"^(?P<slug>(\w|-|\/)*)/", content, name="content"),
